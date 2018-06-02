acf0fe5d54a96c580f45109ca7b1e8f1fd59c9d2
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-02 02:31:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+  array_with_and = []
 
+  if array.length == 1
+    array.join()
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array.each_with_index do |str,idx|
+      unless idx == array.length - 1
+        array_with_and << str
+      else
+        array_with_and << "and #{str}"
+      end
+    end
+    array_with_and.join(", ")
+  end
+  
 end
